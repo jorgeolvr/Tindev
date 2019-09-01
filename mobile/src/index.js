66250a8739f50ea7a5636d857100dffec35b2d03
Jorge Oliveira <jorgeallancastro@icloud.com>
2019-09-01 13:17:22 -0300

    Tela de login estilizada

---
+++ mobile/src/index.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet
+} from 'react-native';
+
+import Login from './pages/Login'
+
+export default function App() {
+  return (
+    <Login />
+  )
+}
