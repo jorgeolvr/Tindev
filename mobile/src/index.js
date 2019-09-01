204894568643f93c3c03554842c320c0e415c05d
Jorge Oliveira <jorgeallancastro@icloud.com>
2019-09-01 15:23:13 -0300

    Tela principal e tela de login finalizada

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,15 +1,10 @@
 
 import React from 'react';
-import {
-  View,
-  Text,
-  StyleSheet
-} from 'react-native';
 
-import Login from './pages/Login'
+import Routes from './routes'
 
 export default function App() {
   return (
-    <Login />
+    <Routes />
   )
 }
--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,19 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import AsyncStorage from '@react-native-community/async-storage'
 import { KeyboardAvoidingView, Platform, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native'
 
+import api from '../services/api'
 import logo from '../assets/logo.png'
 
-export default function Login() {
+export default function Login({ navigation }) {
+  const [user, setUser] = useState('')
+
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user => {
+      if (user) {
+        navigation.navigate('Main', { user })
+      }
+    })
+  }, [])
+
+  async function handleLogin() {
+    const response = await api.post('/devs', { username: user })
+    const { _id } = response.data
+
+    await AsyncStorage.setItem('user', _id)
+
+    navigation.navigate('Main', { user: _id })
+  }
+
   return (
     <KeyboardAvoidingView behavior="padding" enabled={Platform.OS === 'ios'} style={styles.container}>
       <Image source={logo} />
       <TextInput style={styles.input}
-        autoCaptalize="none"
+        autoCapitalize="none"
         autoCorrect={false}
         placeholderTextColor="#999"
-        placeholder="Digite seu usuário no GitHub"/>
-      
-      <TouchableOpacity style={styles.button}>
+        placeholder="Digite seu usuário no GitHub"
+        value={user}
+        onChangeText={setUser}
+      />
+      <TouchableOpacity onPress={handleLogin} style={styles.button}>
         <Text style={styles.buttonText}>Enviar</Text>
       </TouchableOpacity>
     </KeyboardAvoidingView>
